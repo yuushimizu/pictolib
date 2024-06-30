47127f10a23db2e894b0a1c2612c43ed9211434f
Yuu Shimizu <u_u@yuushimizu.com>
2024-07-01 00:06:49 +0900

    Fix example to draw battery

--- examples/lib/index.js
+++ examples/lib/index.js
@@ -1,5 +1,11 @@
 import { create } from "pictolib";
-export const battery = create({ viewBox: [100, 200, 300, 400], stroke: "navy", fill: "transparent", strokeWidth: 1 })
-    .group({ stroke: "gray" }, (g) => g.rect(100, 200, 300, 400))
-    .group({ strokeWidth: 10 }, (g) => g.group({ fill: "navy" }, (g) => g.rect(150, 210, 200, 40)).rect(110, 250, 280, 340));
+export const battery = create({
+    viewBox: [100, 200, 300, 400],
+    stroke: "#444",
+    fill: "transparent",
+    strokeWidth: 10,
+})
+    .group({ fill: "#444" }, (g) => g.rect(170, 210, 160, 40))
+    .rect(110, 250, 280, 340)
+    .group({ stroke: "transparent", fill: "#8d2" }, (g) => [...Array(3).keys()].reduce((g, i) => g.rect(125, 265 + i * 107.5, 250, 95), g));
 //# sourceMappingURL=index.js.map
