48e84350a1450b7fcde261ddfbb1cac869641e34
Yuu Shimizu <u_u@yuushimizu.com>
2024-07-22 15:55:45 +0900

    Fix normalizeAngle

--- src/coord.ts
+++ src/coord.ts
@@ -14,5 +14,5 @@ export type Rect = Readonly<{
 }>;
 
 export const normalizeAngle = (angle: number): number => {
-  return (((angle % Math.PI) * 2 + Math.PI * 2) % Math.PI) * 2;
+  return ((angle % (Math.PI * 2)) + Math.PI * 2) % (Math.PI * 2);
 };
